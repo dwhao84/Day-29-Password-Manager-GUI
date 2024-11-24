58f87d6bbf7f24ad88a3ab75cd2307354fb5c922
Dawei84 <dwhao84@gmail.com>
2024-11-24 12:28:30 +0800

    Update tapped_password_generator function.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import tkinter
 from tkinter import *
 from tkinter import Label
+from tkinter import messagebox
+import random
+import pyperclip
+import os
 
 WEBSITE_LABEL_WIDTH = 35
 PASSWORD_WIDTH = 21
@@ -11,6 +15,38 @@ EMAIL = "dwsamurai84@gmail.com"
 # ----- PASSWORD GENERATOR -----
 def tapped_password_generator():
     print("pressed_password_generator")
+    # Password Generator Project
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    # ( Section 1)
+    # for char in range(nr_letters):
+    #     password_list.append(random.choice(letters))
+    # ( Section 2)
+    # for char in range(nr_symbols):
+    #     password_list += random.choice(symbols)
+    password_list += [ random.choice(letters) for _ in range(nr_letters)]
+    password_list += [ random.choice(letters) for _ in range(nr_symbols) ]
+    password_list += [ random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = ""
+    # for char in password_list:
+    #     password += char
+    password += ''.join(char for char in password_list)
+    print(f"Your password is: {password}")
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ----- SAVE PASSWORD -----
 def tapped_add_btn():
@@ -21,17 +57,23 @@ def tapped_add_btn():
     password_data = password_entry.get()
     website_data = website_entry.get()
 
-    DATASET = f'{ email_data } | { password_data } | { website_data } \n'
+    if len(website_data) == 0 or len(password_data) == 0:
+        print(f"Website:{ len(website_data) }, Password: { len(password_data) }")
+        messagebox.showerror(title="Entry is empty", message=f"There are one of entry is empty: Website:{ len(website_data) }, password data: { len(password_data) }")
+    else:
+        is_ok = messagebox.askokcancel(title=website_data, message=f"There are the detail entered: \n Email: {email_data}"
+                                                        f"\n Password: {password_data}, \n Is it ok to save?")
+        if is_ok:
+            DATASET = f'{email_data} | {password_data} | {website_data} \n'
+            # 用with open的寫法 去撰寫dataset.txt。
+            with open("dataset.txt", mode="a") as file:
+                file.write(DATASET)
+                print("dataset.txt wrote it.")
 
-    # 用with open的寫法 去撰寫dataset.txt。
-    with open("dataset.txt", mode="a") as file:
-        file.write(DATASET)
-        print("dataset.txt wrote it.")
-
-    # 用entry.delete()的方法，來清除輸入資料
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
-    print("Clean the Entry width.")
+            # 用entry.delete()的方法，來清除輸入資料
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            print("Clean the Entry width.")
 
 # ----- UI SETUP -----
 window = Tk()
